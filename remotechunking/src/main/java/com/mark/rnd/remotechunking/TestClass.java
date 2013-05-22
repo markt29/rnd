ba50210d34a762e1bcba35480f075fce5f1c9769
mark <mark.torrequemada@gmail.com>
2013-05-22 19:59:05 +0800

    test class

---
+++ remotechunking/src/main/java/com/mark/rnd/remotechunking/TestClass.java
@@ -0,0 +1,13 @@
+package com.mark.rnd.remotechunking;
+
+public class TestClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
