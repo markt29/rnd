47c30a3eaf81ae0e0567b257b3ed23bb897c05b7
mark <mark.torrequemada@gmail.com>
2013-05-22 20:02:31 +0800

    another

---
+++ remotechunking/src/main/java/com/mark/rnd/remotechunking/AnotherTest.java
@@ -0,0 +1,13 @@
+package com.mark.rnd.remotechunking;
+
+public class AnotherTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
